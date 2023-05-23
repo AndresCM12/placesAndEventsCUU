ed71fdb16401153c1ae0d884e600176ecb03d544
Andres Ch <andres@deadline.com.mx>
2023-05-22 20:27:58 -0600

    Added initial files for the project config

---
+++ eventsAndPlacesCUU/eventsAndPlacesCUU/eventsAndPlacesCUUApp.swift
@@ -0,0 +1,17 @@
+//
+//  eventsAndPlacesCUUApp.swift
+//  eventsAndPlacesCUU
+//
+//  Created by Deadline on 22/05/23.
+//
+
+import SwiftUI
+
+@main
+struct eventsAndPlacesCUUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
